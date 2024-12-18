61fb60407a8877a32b95af5517444ebdcbb3c271
Jess Turner <jessica.turner.cs@gmail.com>
2024-12-17 21:14:17 -0500

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import AboutMe from './components/AboutMe.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  
+  <AboutMe/>
+  <br style="color:white"/>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
